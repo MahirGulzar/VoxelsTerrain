6e69dbd738a740ad08e7491d81509e5a56606f0c
MahirGulzar <mahirgulzar@gmail.com>
2018-08-17 12:10:59 +0300

    Added Block mesh generator

---
+++ Assets/Scripts/Chunk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunk: MonoBehaviour{
+
+    public int width;
+
+    private Color[,,] chunkData;
+
+
+    private void Awake()
+    {
+        chunkData = new Color[width, width, width];
+    }
+
+    public void SetBlock(int x, int y, int z, Color blockData)
+    {
+        chunkData[x, y, z] = blockData;
+        SendMessage("BlockUpdate", this);
+    }
+
+
+    public Color GetBlock(int x, int y, int z)
+    {
+        return chunkData[x, y, z];
+    }
+}
---
+++ Assets/Scripts/PerlinChunkGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinChunkGenerator : MonoBehaviour {
+
+    public int seed;
+    private Chunk chunk;
+
+	// Use this for initialization
+	void Start () {
+        chunk = GetComponent<Chunk>();
+
+        Generate(chunk, seed);
+	}
+
+    private void Generate(Chunk chunk, int seed)
+    {
+        Random.InitState(seed);
+
+        for(int x=0;x<chunk.width;x++)
+            for (int y = 0; y < chunk.width; y++)
+                for (int z = 0; z < chunk.width; z++)
+                {
+                    chunk.SetBlock(x, y, z, Color.yellow);
+                }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+
+}
+
+
+
